7786ce05f6d850f93a5e2e010b6f998e1711ac5c
Paul Campbell <pcampbell@kemitix.net>
2017-08-26 11:10:38 +0100

    WrappingPrintStreamTest: tidy up

--- src/test/java/net/kemitix/outputcapture/WrappingPrintStreamsTest.java
+++ src/test/java/net/kemitix/outputcapture/WrappingPrintStreamsTest.java
@@ -17,24 +17,16 @@ public class WrappingPrintStreamsTest {
 
     @Test
     public void requiresAWrapper() {
-        //given
-        final Wrapper<PrintStream> wrapper = null;
-        //then
-        Assertions.assertThatNullPointerException()
-                  .isThrownBy(() ->
-                                      //when
-                                      new WrappingPrintStreams(wrapper))
-                  //and
-                  .withMessage("mainWrapper");
+        assertThatNullPointerException().isThrownBy(() -> new WrappingPrintStreams(null))
+                                        .withMessage("mainWrapper");
     }
 
     @Test
     public void canCreateWithSingleWrapper() {
         //given
-        final Wrapper<PrintStream> wrapper =
-                () -> new WrapperState<>(new PrintStream(new ByteArrayOutputStream()));
+        val wrapper = wrapper();
         //when
-        final WrappingPrintStreams result = new WrappingPrintStreams(wrapper);
+        val result = new WrappingPrintStreams(wrapper);
         //then
         assertThat(result).returns(wrapper, WrappingPrintStreams::getMainWrapper);
         assertThat(result.getOtherWrappers()).isEmpty();
@@ -43,12 +35,10 @@ public class WrappingPrintStreamsTest {
     @Test
     public void canCreateWithTwoWrappers() {
         //given
-        final Wrapper<PrintStream> wrapper1 =
-                () -> new WrapperState<>(new PrintStream(new ByteArrayOutputStream()));
-        final Wrapper<PrintStream> wrapper2 =
-                () -> new WrapperState<>(new PrintStream(new ByteArrayOutputStream()));
+        val wrapper1 = wrapper();
+        val wrapper2 = wrapper();
         //when
-        final WrappingPrintStreams result = new WrappingPrintStreams(wrapper1, wrapper2);
+        val result = new WrappingPrintStreams(wrapper1, wrapper2);
         //then
         assertThat(result).returns(wrapper1, WrappingPrintStreams::getMainWrapper);
         assertThat(result.getOtherWrappers()).containsExactly(wrapper2);
@@ -57,16 +47,17 @@ public class WrappingPrintStreamsTest {
     @Test
     public void canCreateWithThreeWrappers() {
         //given
-        final Wrapper<PrintStream> wrapper1 =
-                () -> new WrapperState<>(new PrintStream(new ByteArrayOutputStream()));
-        final Wrapper<PrintStream> wrapper2 =
-                () -> new WrapperState<>(new PrintStream(new ByteArrayOutputStream()));
-        final Wrapper<PrintStream> wrapper3 =
-                () -> new WrapperState<>(new PrintStream(new ByteArrayOutputStream()));
+        val wrapper1 = wrapper();
+        val wrapper2 = wrapper();
+        val wrapper3 = wrapper();
         //when
-        final WrappingPrintStreams result = new WrappingPrintStreams(wrapper1, wrapper2, wrapper3);
+        val result = new WrappingPrintStreams(wrapper1, wrapper2, wrapper3);
         //then
         assertThat(result).returns(wrapper1, WrappingPrintStreams::getMainWrapper);
         assertThat(result.getOtherWrappers()).containsExactly(wrapper2, wrapper3);
+    }
+
+    private Wrapper<PrintStream> wrapper() {
+        return () -> new WrapperState<>(new PrintStream(new ByteArrayOutputStream()));
     }
 }
