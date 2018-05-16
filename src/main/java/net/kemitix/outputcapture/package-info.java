ba3b4c4bfd046346a8a85211c048d342b144fcf0
Paul Campbell <pcampbell@kemitix.net>
2018-05-16 23:10:44 +0100

    Fix: JavadocStyle

--- src/main/java/net/kemitix/outputcapture/package-info.java
+++ src/main/java/net/kemitix/outputcapture/package-info.java
@@ -19,4 +19,8 @@
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+/**
+ * Capture output written to {@code System.out} and {@code System.err}.
+ */
+
 package net.kemitix.outputcapture;
