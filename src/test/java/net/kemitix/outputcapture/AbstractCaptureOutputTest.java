ff1736916b3e21578b588ad5f97e345d34b2eaf1
Paul Campbell <pcampbell@kemitix.net>
2018-05-18 18:34:36 +0100

    Add tests for removing all interceptors

---
+++ src/test/java/net/kemitix/outputcapture/AbstractCaptureOutputTest.java
@@ -0,0 +1,82 @@
+package net.kemitix.outputcapture;
+
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AbstractCaptureOutputTest {
+
+    @Test
+    public void givenNoInteceptorsWhenRemoveAllThenNoneLeft() {
+        //given
+        //when
+        AbstractCaptureOutput.removeAllInterceptors();
+        //then
+        assertThat(AbstractCaptureOutput.activeCount()).isZero();
+    }
+
+    @Test
+    public void givenOneInterceptorWhenRemoveAllThenNoneLeft() throws InterruptedException {
+        //given
+        final MyAsyncCapture capture = new MyAsyncCapture();
+        assertThat(AbstractCaptureOutput.activeCount()).isNotZero();
+        //when
+        AbstractCaptureOutput.removeAllInterceptors();
+        //then
+        capture.remove();
+        assertThat(AbstractCaptureOutput.activeCount()).isZero();
+    }
+
+    @Test
+    public void givenTwoInterceptorsWhenRemoveAllThenNoneLeft() throws InterruptedException {
+        //given
+        final MyAsyncCapture capture1 = new MyAsyncCapture();
+        final MyAsyncCapture capture2 = new MyAsyncCapture();
+        assertThat(AbstractCaptureOutput.activeCount()).isNotZero();
+        //when
+        AbstractCaptureOutput.removeAllInterceptors();
+        //then
+        capture1.remove();
+        capture2.remove();
+        assertThat(AbstractCaptureOutput.activeCount()).isZero();
+    }
+
+    private class MyAsyncCapture {
+
+        private SimpleLatch finished = new SimpleLatch();
+        private CountDownLatch completed;
+        private OngoingCapturedOutput capture;
+
+        MyAsyncCapture() throws InterruptedException {
+            final CountDownLatch started = new SimpleLatch();
+            capture = Captors.asyncCopyAll().capture(() -> {
+                release(started);
+                await(finished);
+            });
+            await(started);
+            completed = capture.getCompletedLatch();
+        }
+
+        void remove() throws InterruptedException {
+            finished.countDown();
+            await(completed);
+        }
+    }
+
+    private void release(final CountDownLatch latch) {
+        latch.countDown();
+    }
+
+    private void await(final CountDownLatch latch) throws InterruptedException {
+        latch.await(1, TimeUnit.SECONDS);
+    }
+
+    private class SimpleLatch extends CountDownLatch {
+        SimpleLatch() {
+            super(1);
+        }
+    }
+}
