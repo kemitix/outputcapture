15017c65b4ad75698fbe6689e6a6b36d2f6faef8
Paul Campbell <pcampbell@kemitix.net>
2018-05-25 19:41:19 +0100

    SafeLatchTest: rewrite to not use Thread.sleep()

--- src/test/java/net/kemitix/outputcapture/SafeLatchTest.java
+++ src/test/java/net/kemitix/outputcapture/SafeLatchTest.java
@@ -2,6 +2,8 @@ package net.kemitix.outputcapture;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,23 +16,26 @@ public class SafeLatchTest {
         final Long maxAwaitMilliseconds = 100L;
         final AtomicBoolean interrupted = new AtomicBoolean(false);
         final SafeLatch safeLatch = new SafeLatch(1, maxAwaitMilliseconds, () -> interrupted.set(true));
+        final CountDownLatch latch = new CountDownLatch(1);
         //when
         new Thread(() -> {
             final Thread thread = Thread.currentThread();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(10L);
-                    } catch (InterruptedException e) {
-                        // ignore
-                    }
-                    thread.interrupt();
-                }
-            }).start();
+            waitThenInterrupt(thread, maxAwaitMilliseconds, latch);
+            latch.countDown();
             safeLatch.await();
         }).start();
         //then
         assertThat(interrupted).isTrue();
+    }
+
+    private void waitThenInterrupt(final Thread thread, final long millis, final CountDownLatch latch) {
+        new Thread(() -> {
+            try {
+                latch.await(millis, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                // ignore
+            }
+            thread.interrupt();
+        }).start();
     }
 }
